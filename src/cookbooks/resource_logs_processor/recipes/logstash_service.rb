76dc30ba31bfc28b16c61a1440619364cef17fc1
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-02-04 21:34:58 +1300

    Half-assed version of the service. Need to handle the memory thing
    references #1

--- src/cookbooks/resource_logs_processor/recipes/logstash_service.rb
+++ src/cookbooks/resource_logs_processor/recipes/logstash_service.rb
@@ -19,7 +19,10 @@ end
 # SYSTEMD SERVICE
 #
 
-# From here: https://logstash.io/blog/2016/11/21/gc-tuning/
+jenkins_http_port = node['jenkins']['port']['http']
+jenkins_slave_agent_port = node['jenkins']['port']['slave']
+
+# From here: https://jenkins.io/blog/2016/11/21/gc-tuning/
 java_server_args = '-server -XX:+AlwaysPreTouch'
 java_gc_args =
   '-XX:+UseConcMarkSweepGC' \
@@ -35,12 +38,26 @@ java_awt_args = '-Djava.awt.headless=true'
 # Make sure java prefers IPv4 over IPv6 because Jolokia doesn't like IPv6
 java_ipv4_args = '-Djava.net.preferIPv4Stack=true'
 
-# Settings (from here: https://wiki.logstash-ci.org/display/logstash/Features+controlled+by+system+properties)
-logstash_java_args = ''
+# Settings (from here: https://wiki.jenkins-ci.org/display/JENKINS/Features+controlled+by+system+properties)
+# -hudson.model.UpdateCenter.never -> never download new jenkins versions
+# -hudson.model.DownloadService.never -> never download new plugin information
+# -jenkins.model.Jenkins.slaveAgentPort -> set the port for the slaves to connect to jenkins
+# -jenkins.model.Jenkins.slaveAgentPortEnforce -> enforce the slave connection port. Cannot change it from the UI.
+# -jenkins.CLI.disabled -> Disable the CLI through JNLP and HTTP
+# -jenkins.install.runSetupWizard -> Skip the install wizzard
+jenkins_java_args =
+  '-Dhudson.model.UpdateCenter.never=true' \
+  ' -Dfile.encoding=UTF8' \
+  ' -Dhudson.model.DownloadService.never=true' \
+  " -Djenkins.model.Jenkins.slaveAgentPort=#{jenkins_slave_agent_port}" \
+  ' -Djenkins.model.Jenkins.slaveAgentPortEnforce=true' \
+  ' -Djenkins.CLI.disabled=true' \
+  ' -Djenkins.install.runSetupWizard=false' \
+  ' -Dorg.apache.commons.jelly.tags.fmt.timeZone=Pacific/Auckland'
 
 # Turn on GC logging
 java_diagnostics =
-  '-Xloggc:/var/log/logstash_gc-%t.log' \
+  '-Xloggc:/var/log/jenkins_gc-%t.log' \
   ' -XX:NumberOfGCLogFiles=10' \
   ' -XX:+UseGCLogFileRotation' \
   ' -XX:GCLogFileSize=25m' \
@@ -54,12 +71,30 @@ java_diagnostics =
   ' -XX:+PrintAdaptiveSizePolicy' \
   ' -XX:+HeapDumpOnOutOfMemoryError'
 
-logstash_user = node['logstash']['service_user']
-logstash_war_path = node['logstash']['path']['war_file']
-logstash_pid_file = node['logstash']['path']['pid_file']
+# Set the Jolokia jar as an agent so that we can export the JMX metrics to influx
+# For the settings see here: https://jolokia.org/reference/html/agents.html#agents-jvm
+jolokia_jar_path = node['jolokia']['path']['jar_file']
+jolokia_agent_host = node['jolokia']['agent']['host']
+jolokia_agent_port = node['jolokia']['agent']['port']
+jenkins_metrics_args =
+  "-javaagent:#{jolokia_jar_path}=" \
+  'protocol=http' \
+  ",host=#{jolokia_agent_host}" \
+  ",port=#{jolokia_agent_port}" \
+  ',discoveryEnabled=false'
 
-run_logstash_script = '/usr/local/logstash/run_logstash.sh'
-file run_logstash_script do
+# Set jenkins to be served at http://localhost:8080/builds
+proxy_path = node['jenkins']['proxy_path']
+jenkins_args =
+  "--httpPort=#{jenkins_http_port}" \
+  " --prefix=/#{proxy_path}"
+
+jenkins_user = node['jenkins']['service_user']
+jenkins_war_path = node['jenkins']['path']['war_file']
+jenkins_pid_file = node['jenkins']['path']['pid_file']
+
+run_jenkins_script = '/usr/local/jenkins/run_jenkins.sh'
+file run_jenkins_script do
   action :create
   content <<~SH
     #!/bin/sh
@@ -99,48 +134,51 @@ file run_logstash_script do
         echo "Maximum memory for VM set to ${max_mem}. Setting max memory for java to ${mx} Mb"
       fi
 
-      user_java_opts="#{java_server_args} #{java_gc_args} #{java_awt_args} #{java_ipv4_args} #{logstash_java_args}"
-      user_java_jar_opts="#{logstash_args}"
+      user_java_opts="#{java_server_args} #{java_gc_args} #{java_awt_args} #{java_ipv4_args} #{jenkins_java_args}"
+      user_java_jar_opts="#{jenkins_args}"
 
-      echo nohup java ${user_java_opts} ${java_max_memory} #{java_diagnostics} #{logstash_metrics_args} -jar #{logstash_war_path} ${user_java_jar_opts}
-      nohup java ${user_java_opts} ${java_max_memory} #{java_diagnostics} #{logstash_metrics_args} -jar #{logstash_war_path} ${user_java_jar_opts} 2>&1 &
-      echo "$!" >"#{logstash_pid_file}"
+      echo nohup java ${user_java_opts} ${java_max_memory} #{java_diagnostics} #{jenkins_metrics_args} -jar #{jenkins_war_path} ${user_java_jar_opts}
+      nohup java ${user_java_opts} ${java_max_memory} #{java_diagnostics} #{jenkins_metrics_args} -jar #{jenkins_war_path} ${user_java_jar_opts} 2>&1 &
+      echo "$!" >"#{jenkins_pid_file}"
     }
 
     # =============================================================================
     # Fire up
     startup
   SH
-  group node['logstash']['service_group']
+  group node['jenkins']['service_group']
   mode '0550'
-  owner node['logstash']['service_user']
+  owner node['jenkins']['service_user']
 end
 
-logstash_service_name = node['logstash']['service_name']
-logstash_environment_file = node['logstash']['path']['environment_file']
-systemd_service logstash_service_name do
+jenkins_service_name = node['jenkins']['service_name']
+jenkins_environment_file = node['jenkins']['path']['environment_file']
+systemd_service jenkins_service_name do
   action :create
   install do
     wanted_by %w[multi-user.target]
   end
   service do
-    environment_file logstash_environment_file
-    exec_reload "/usr/bin/curl http://localhost:#{logstash_http_port}/#{proxy_path}/reload"
-    exec_start run_logstash_script
-    exec_stop "/usr/bin/curl http://localhost:#{logstash_http_port}/#{proxy_path}/safeExit"
-    pid_file logstash_pid_file
-    restart 'on-failure'
+    # Load env vars from /etc/default/ and /etc/sysconfig/ if they exist.
+    # Prefixing the path with '-' makes it try to load, but if the file doesn't
+    # exist, it continues onward.
+    environment_file '-/etc/default/logstash'
+    environment_file '-/etc/sysconfig/logstash'
+    exec_start /usr/share/logstash/bin/logstash "--path.settings" "/etc/logstash"
+    limit_nofile 16384
+    nice 19
+    restart 'always'
     type 'forking'
-    user logstash_user
+    user jenkins_user
   end
   unit do
     after %w[network-online.target]
-    description 'logstash CI system'
-    documentation 'https://logstash.io'
+    description 'Logstash'
+    documentation 'https://elastic.co'
     requires %w[network-online.target]
   end
 end
 
-service logstash_service_name do
+service 'logstash' do
   action :disable
 end
